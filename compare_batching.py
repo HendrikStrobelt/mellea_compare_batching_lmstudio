c4d0bed768eafc1da73bf319bc8533809a12911f
Hendrik Strobelt <hendrik.strobelt@ibm.com>
2026-03-28 16:41:06 -0400

    cleanup

--- compare_batching.py
+++ compare_batching.py
@@ -1,5 +1,6 @@
 import argparse
 import asyncio
+import os
 import time
 
 from mellea import MelleaSession
@@ -7,8 +8,7 @@ from mellea.backends.model_options import ModelOption
 from mellea.backends.openai import OpenAIBackend
 from mellea.core import FancyLogger
 
-# LMSTUDIO_BASE_URL = "http://192.222.59.234:8011/v1"
-LMSTUDIO_BASE_URL = "http://127.0.0.1:1234/v1"
+LMSTUDIO_BASE_URL = os.environ.get("COMPARE_LMSTUDIO_BASE_URL", "http://127.0.0.1:1234/v1")
 # MODEL_ID = "gpt-oss-20b"
 MODEL_ID="granite-4.0-micro@q8_0"
 
